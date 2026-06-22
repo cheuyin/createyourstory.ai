dea90494a706593bef326c6544405e125bdd6eea
cheuyin <yinstanleycheung@gmail.com>
2026-06-21 17:16:09 -0700

    feat: create functions for creating db tables + session

---
+++ backend/db/database.py
@@ -0,0 +1,13 @@
+from core.config import settings
+from sqlmodel import create_engine, Session, SQLModel
+
+engine = create_engine(settings.DATABASE_URL)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
